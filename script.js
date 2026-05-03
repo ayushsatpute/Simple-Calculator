ad906224ad29ec0353b57235741e504fcfcdaf49
Ayush <satputeayush588@gmail.com>
2026-05-03 14:00:37 +0530

    Day 1: Calculator completed with clean layout and fixed equal button

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-let d=document.getElementById("display");
+const d = document.getElementById("display");
 
 function write(v){
-if(d.value==="Error") d.value="";
-d.value+=v;
+if(d.value === "Error") d.value = "";
+d.value += v;
 }
 
 function reset(){
-d.value="";
+d.value = "";
 }
 
 function remove(){
-d.value=d.value.slice(0,-1);
+d.value = d.value.slice(0,-1);
 }
 
 function solve(){
 try{
-if(d.value==="") return;
-d.value=Function("return "+d.value)();
+if(d.value === "") return;
+d.value = eval(d.value);
 }catch{
-d.value="Error";
+d.value = "Error";
 }
 }
 
-document.addEventListener("keydown",e=>{
+document.addEventListener("keydown", (e)=>{
 if(!isNaN(e.key)) write(e.key);
 if(["+","-","*","/"].includes(e.key)) write(e.key);
-if(e.key==="Enter") solve();
-if(e.key==="Backspace") remove();
-if(e.key===".") write(".");
-if(e.key==="Escape") reset();
+if(e.key === "Enter") solve();
+if(e.key === "Backspace") remove();
+if(e.key === ".") write(".");
+if(e.key === "Escape") reset();
 });
